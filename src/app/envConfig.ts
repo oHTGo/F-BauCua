b51a028e8a57d3244316d477b64086b9f18d0ffd
oHTGo <nguyennhathuy.orit@gmail.com>
2022-01-18 22:57:54 +0700

    Add array url cors

--- src/app/envConfig.ts
+++ src/app/envConfig.ts
@@ -1,7 +1,7 @@
 interface IEnvConfig {
   // Server
   NODE_ENV: string;
-  FRONTEND_URL: string;
+  FRONTEND_URL: string[];
   API_PORT: string;
   SOCKET_PORT: string;
 
@@ -16,9 +16,18 @@ interface IEnvConfig {
   REDIS_URI: string;
 }
 
+const getFrontendUrls = (): string[] => {
+  try {
+    const urls = String(process.env.FRONTEND_URL).split(',');
+    return urls;
+  } catch (err) {
+    return ['http://localhost:5500'];
+  }
+};
+
 export default (): IEnvConfig => ({
   NODE_ENV: process.env.NODE_ENV || 'development',
-  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5500',
+  FRONTEND_URL: getFrontendUrls(),
   API_PORT: process.env.API_PORT || '5000',
   SOCKET_PORT: process.env.SOCKET_PORT || '3000',
 
